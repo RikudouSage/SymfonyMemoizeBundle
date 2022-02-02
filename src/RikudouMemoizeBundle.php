09f8db71c5b92bd989784eb3e1665fa5c7b81565
Dominik Chrastecky <dominik@chrastecky.cz>
2022-02-03 00:09:12 +0100

    Remove non-working autoloader

--- src/RikudouMemoizeBundle.php
+++ src/RikudouMemoizeBundle.php
@@ -18,21 +18,6 @@ final class RikudouMemoizeBundle extends Bundle
             "{$projectDir}/memoized",
         );
 
-        $targetDir = $container->getParameter('rikudou.memoize.target_dir');
-        assert(is_string($targetDir));
-
-        spl_autoload_register(function (string $className) use ($targetDir) {
-            if (!str_starts_with($className, 'App\\Memoized')) {
-                return;
-            }
-
-            $path = substr($className, strlen('App\\Memoized\\'));
-            $path = "{$targetDir}/{$path}.php";
-            if (file_exists($path)) {
-                require $path;
-            }
-        });
-
         $container->addCompilerPass(new MemoizeProxyCreatorCompilerPass());
     }
 }
